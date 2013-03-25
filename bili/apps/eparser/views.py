81b9e6ab884d1b63116df335cd33fad67a40638a
DanielBlack <danielblack@danielblack.name>
2013-03-25 20:47:03 +0800

    Add multi parse module, Use gevent.spawn to parse concurrently

--- bili/apps/eparser/views.py
+++ bili/apps/eparser/views.py
@@ -3,7 +3,8 @@ from django.contrib.auth import login, logout, authenticate
 from django.contrib.auth.models import User
 from django.http import HttpResponse
 
-from eparser import get_parser, QueryException
+from eparser import get_parser, QueryError, ParserNotFound
+import gevent
 from utils.decorators import check_method
 
 import simplejson as json
@@ -17,22 +18,50 @@ def get_price(request, ename):
     context = {'status' : 200}
     data = {'errors': []}
     context['data'] = data
-    parser = get_parser(ename)()
     keyword = request.GET.get('k', None)
-    if not parser or not keyword:
+    if not keyword:
         data['success'] = False
-        data['errors'].append(u'参数错误')
+        data['errors'].append(u'关键词不能为空')
     else:
         try:
-            price = parser.parse(keyword)
-        except QueryException:
+            value = parse(ename, keyword)
+        except QueryError:
             data['success'] = False
             data['errors'].append(u'查询失败')
         else:
-            data['price'] = price
+            data[ename]['value'] = value
 
     return HttpResponse(
             json.dumps(context),
             mimetype=u'application/json',
     )
+
+
+@check_method('GET')
+def get_mul_price(request):
+    '''
+    search the price in the multiple eshop
+    '''
+    context = {'status' : 200}
+    data = {'errors': []}
+    context['data'] = data
+    keyword = request.GET.get('k', None)
+    if not keyword:
+        data['success'] = False
+        data['errors'].append(u'关键词不能为空')
+    else:
+        enames = (set(request.GET.keys())).intersection(PARSERS_DICT.keys())
+        jobs = [gevent.spawn(parser.parse, ename, k) for ename in enames]
+        gevent.joinall(jobs, time_out=5)
+        for i in len(jobs):
+            if jobs[i].exception:
+                data[enames[i]]['error'] = jobs[i].exception.message
+            else:
+                data[enames[i]]['value'] = jobs[i].value
+                
+    return HttpResponse(
+            json.dumps(context),
+            mimetype=u'application/json',
+    )
+
 

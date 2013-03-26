c0585b46f15e7866c0211b8dfafcd59cc1f2100c
DanielBlack <danielblack@danielblack.name>
2013-03-26 21:27:30 +0800

    Modify UI

--- bili/statics/js/home.js
+++ bili/statics/js/home.js
@@ -1,6 +1,21 @@
+var SEARCH_URL = 'search/'
 $('document').ready(function(){
     $('#cmp-btn').click(function(){
-        var value = $('#cmp-input').attr('value');
-        $.get
+        alert("hehe");
+        var data = {
+            k: $('#cmp-input').attr('value'),
+        };
+        $("#checkbox-wrap input[type='checkbox']").each(function(){
+            if($(this).prop("checked")){
+                data[$(this).attr("value")] = null;
+            }
+        })
+        $.getJSON(
+            SEARCH_URL,
+            data,
+            function(json){
+                alert(json.data.value.tmall.price);
+            }
+        );
     });
 });

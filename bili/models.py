7557eb42a06b11baaa9fe3147eab33945a2b82b5
DanielBlack <danielblack@danielblack.name>
2013-05-22 01:21:50 +0800

    Add `Interest` model

--- bili/models.py
+++ bili/models.py
@@ -1,8 +1,10 @@
-from sqlalchemy import Table, Column, ForeignKey, Integer, String
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import create_engine
-from sha import sha
 from config import *
+from datetime import datetime
+from sqlalchemy import Table, Column, ForeignKey, Integer, String, create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship, backref
+from sqlalchemy.types import DateTime, Float
+from sha import sha
 
 __all__ = ['User', 'engine']
 
@@ -43,6 +45,40 @@ class User(Base):
 
     def __repr__(self):
         return "<User, %s>" % self.username
+
+class Interest(Base):
+    '''
+    The goods user interested in
+    '''
+    __tablename__ = 'interests'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(255))
+    updatetime = Column(DateTime)
+    user_id = Column(Integer, ForeignKey('users.id'))
+    price = Column(Float)
+    img_url = Column(String(2048))
+
+    user = relationship("User", backref=backref('interests', order_by=id))
+
+    def __init__(self, name, user, price, img_url):
+        self.name = name
+        self.updatetime = datetime()
+        self.user = user
+        self.user_id = user_id
+        self.price = price
+        self.img_url = img_url
+
+    def update_price(self):
+        '''
+        TODO
+        Update all the goods' information
+        '''
+        pass
+
+    def __repr__(self):
+        return "<Interest, %s, %s>" % (self.user.username, self.name)
+
 
 
 if __name__ == '__main__':
